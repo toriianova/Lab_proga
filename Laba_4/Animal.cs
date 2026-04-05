a145e154b77f5e0ff8f71256bc252870b5c6765b
Victoria LOGINOVA <viktoriia8035@gmail.com>
2026-04-05 20:05:47 +0300

    додано клас Animal

---
+++ Laba_4/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_4
+{
+    internal class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+            Console.WriteLine("Викликано конструктор Animal");
+        }
+
+        public void ShowName()
+        {
+            Console.WriteLine($"Ім'я тварини: {Name}");
+        }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Тварина видає звук");
+        }
+
+        public virtual void Run()
+        {
+            Console.WriteLine("Тварина біжить");
+        }
+    }
+}
