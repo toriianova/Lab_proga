082ccd31f52b3a54f528c04050a2bab06a006775
Victoria LOGINOVA <viktoriia8035@gmail.com>
2026-04-05 21:57:16 +0300

    змінила помилку в класах Table i Laptop, додала switch-case для вибору завдання + прописала код для магазину електроніки

--- Laba_4/Device.cs
+++ Laba_4/Device.cs
@@ -32,42 +32,49 @@ namespace Laba_4
                 return price > 45000;
             }
 
-            protected int Code => code;
+            public double Price
+            {
+                get
+                {
+                    return price;
+                }
+            }
+        protected int Code => code;
             protected string Model => model;
-            protected double Price => price;
 
             public static int GetCount()
             {
                 return count;
             }
-            class Laptop : Device
-            {
-                private double screenDiagonal;
-
-                public Laptop(int code, string model, double price, double screenDiagonal) : base(code, model, price)
-                {
-                    this.screenDiagonal = screenDiagonal;
-                }
-
-                public override void GetInfo()
-                {
-                    Console.WriteLine($"[Ноутбук] Код: {Code}, Модель: {Model}, Ціна: {Price} грн, Діагональ: {screenDiagonal} дюймів");
-                }
-            }
-            class Tablet : Device
-            {
-                private bool simSupport;
-
-                public Tablet(int code, string model, double price, bool simSupport) : base(code, model, price)
-                {
-                    this.simSupport = simSupport;
-                }
-
-                public override void GetInfo()
-                {
-                    string simText = simSupport ? "Так" : "Ні";
-                    Console.WriteLine($"[Планшет] Код: {Code}, Модель: {Model}, Ціна: {Price} грн, Підтримка SIM: {simText}");
-                }
-            }
     }
+        class Laptop : Device
+        {
+           private double screenDiagonal;
+
+           public Laptop(int code, string model, double price, double screenDiagonal) : base(code, model, price)
+           {
+             this.screenDiagonal = screenDiagonal;
+           }
+
+           public override void GetInfo()
+           {
+             Console.WriteLine($"[Ноутбук] Код: {Code}, Модель: {Model}, Ціна: {Price} грн, Діагональ: {screenDiagonal} дюймів");
+           }
+        }        
+        class Tablet : Device
+        {
+            private bool simSupport;
+
+            public Tablet(int code, string model, double price, bool simSupport) : base(code, model, price)
+            {
+                this.simSupport = simSupport;
+            }
+
+            public override void GetInfo()
+            {
+                string simText = simSupport ? "Так" : "Ні";
+                Console.WriteLine($"[Планшет] Код: {Code}, Модель: {Model}, Ціна: {Price} грн, Підтримка SIM: {simText}");
+            }
+        }
+
 }
--- Laba_4/Program.cs
+++ Laba_4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
 
 namespace Laba_4
 {
@@ -8,7 +9,30 @@ namespace Laba_4
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Puppy puppy = new Puppy("Трой");
+            Console.WriteLine("Вибір завдання:");
+            Console.WriteLine("1 - Puppy / Animals");
+            Console.WriteLine("2 - Магазин електроніки");
+            Console.Write("Ваш вибір: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    RunPuppy();
+                    break;
+
+                case "2":
+                    ElectronicsStore();
+                    break;
+
+                default:
+                    Console.WriteLine("Невірний вибір!");
+                    break;
+            }
+        }
+
+        static void RunPuppy()
+        { Puppy puppy = new Puppy("Трой");
 
             puppy.ShowName();
             puppy.MakeSound();
@@ -17,6 +41,33 @@ namespace Laba_4
             puppy.Bite();
 
             Console.WriteLine(puppy.ToString());
+        }
+
+        static void ElectronicsStore()
+        {
+            List<Device> devices = new List<Device>();
+
+            // Створюємо девайси
+            devices.Add(new Laptop(101, "Asus ROG", 50000, 15.6));
+            devices.Add(new Laptop(102, "HP Pavilion", 40000, 14));
+            devices.Add(new Tablet(261, "iPad Pro", 60000, true));
+            devices.Add(new Tablet(292, "Samsung Tab", 30000, false));
+
+            double totalPrice = 0;
+            int expensiveCount = 0;
+
+            // Виводимо інформацію та підраховуємо
+            foreach (var device in devices)
+            {
+                device.GetInfo();
+                totalPrice += device.Price;
+                if (device.IsExpensive())
+                    expensiveCount++;
+            }
+
+            Console.WriteLine($"\nЗагальна кількість девайсів: {Device.GetCount()}");
+            Console.WriteLine($"Загальна сума цін: {totalPrice} грн");
+            Console.WriteLine($"Кількість дорогих девайсів (>45000 грн): {expensiveCount}");
         }
     }
 }
